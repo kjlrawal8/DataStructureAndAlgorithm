5cdfe0fb69efc4d18d67225d92fdf8ff2fd19e5b
kjlrawal8 <72538972+kjlrawal8@users.noreply.github.com>
2023-07-07 20:01:53 -0500

    Create MinimumCut.java

---
+++ DynamicProgramming/MinimumCut.java
@@ -0,0 +1,23 @@
+public int minCut(String s) {
+        int n = s.length();
+        mcmDpMatrix = new int[n+1][n+1];
+        Arrays.stream(mcmDpMatrix).forEach(row -> Arrays.fill(row, -1));
+        return minCutSolve(s, 0, n-1);
+    }
+
+    private int minCutSolve(String s, int i, int j) {
+        if(i >= j || isPalindrome(s, i, j))
+            return 0;
+        if(mcmDpMatrix[i][j] != -1)
+            return mcmDpMatrix[i][j];
+
+        mcmDpMatrix[i][j] = Integer.MAX_VALUE;
+
+        for (int k = i; k < j; k++) {
+            if(isPalindrome(s,i,k)){
+                int temp = Math.min(mcmDpMatrix[i][j], minCutSolve(s, i, k) + minCutSolve(s,k+1, j) + 1);
+                mcmDpMatrix[i][j] = temp;
+            }
+        }
+        return mcmDpMatrix[i][j];
+    }
