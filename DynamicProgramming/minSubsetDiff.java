b3a652d8b6c864e686a8bfe490a4052b1ad40cb5
kjlrawal8 <72538972+kjlrawal8@users.noreply.github.com>
2023-07-07 19:48:29 -0500

    Update minSubsetDiff.java

--- DynamicProgramming/minSubsetDiff.java
+++ DynamicProgramming/minSubsetDiff.java
@@ -18,3 +18,19 @@ public static int findMin(int arr[], int n) {
             }
         return mn;
     }
+private static void findValidSubset(int[] arr, int n, int range) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= range; j++) {
+                if(i == 0)
+                    booleanSubsetMatrix[i][j] = false;
+                else if(j == 0)
+                    booleanSubsetMatrix[i][j] = true;
+                else if(arr[i-1] <= j){
+                    booleanSubsetMatrix[i][j] = booleanSubsetMatrix[i-1][j- arr[i-1]] || booleanSubsetMatrix[i-1][j];
+                }
+                else
+                    booleanSubsetMatrix[i][j] = booleanSubsetMatrix[i-1][j];
+            }
+        }
+    }
+
